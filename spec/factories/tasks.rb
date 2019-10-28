bf18205627d76979892fdc92136dd89f0ec2fae6
knykd <kok1022kok@gmail.com>
2019-10-28 19:11:49 +0900

    新規:model spec作成

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -1,5 +1,16 @@
 FactoryBot.define do
   factory :task do
-    
+    title { 'test' }
+    status { 'todo' }
+  end
+
+  factory :empty_task, class: Task do
+    title { '' }
+    status { '' }
+  end
+
+  factory :same_task, class: Task do
+    title { 'test' }
+    status { 'done' }
   end
 end
--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe Task, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let!(:task) { FactoryBot.create(:task) }
+  let(:empty_task) { FactoryBot.build(:empty_task) }
+  let(:same_task) { FactoryBot.build(:same_task) }
+
+  # 空の場合無効
+  context 'empty params' do
+    it 'invalid without a title' do
+      expect(empty_task.valid?).to eq(false)
+    end
+
+    it 'invalid without a status' do
+      expect(empty_task.valid?).to eq(false)
+    end
+  end
+
+  # 重複する場合無効
+  context 'same params' do
+    it 'invalid with a duplicate title' do
+      expect(same_task.valid?).to eq(false)
+    end
+  end
 end
